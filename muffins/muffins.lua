24c5cd21dc777035e16f2c366693dc8ec40e7233
Ender <60693808+Tyverion@users.noreply.github.com>
2026-01-08 14:54:42 -0700

    Update muffins.lua

--- muffins/muffins.lua
+++ muffins/muffins.lua
@@ -7,11 +7,10 @@ packets = require('packets')
 
 local zone           = 0
 local lastZone       = 0
-local baselineZone   = 235 -- Kamihr Drifts
-local validZones     = S{275, 133, 189, 107} -- Three zones for Sortie
+local baselineZone   = 267 -- Kamihr Drifts
+local validZones     = S{275, 133, 189} -- Three zones for Sortie
 local initialGalli   = 1
 local totalGalli     = 0
-local updated        = false
 local reportComplete = false
 local baselineRequested = false
 local exitRequested = false
@@ -22,17 +21,18 @@ local file_path = windower.addon_path .. 'data/'
 
 windower.register_event('load', function()
     -- Get the current zone on load
-    baselineZone = windower.ffxi.get_info().zone
+    if windower.ffxi.get_info().zone == baselineZone then
 
-    if not windower.dir_exists(file_path) then
-        windower.create_dir(file_path)
+        if not windower.dir_exists(file_path) then
+            windower.create_dir(file_path)
+        end
+
+        -- Capture baseline on load.
+        packets.inject(packets.new('outgoing', 0x115))
+        reportComplete = false
+        baselineRequested = true
+        exitRequested = false
     end
-
-    -- Capture baseline on load.
-    packets.inject(packets.new('outgoing', 0x115))
-    reportComplete = false
-    baselineRequested = true
-    exitRequested = false
 
 end)
 
@@ -40,16 +40,20 @@ windower.register_event('zone change', function(new,old)
     -- Whenever we change zone we want to know if it isn't Sortie so we can later calculate the difference.
     newZone = new
     oldZone = old
-    -- Entering Kamihr Drifts from a non-Sortie zone establishes the baseline.
-    if not validZones:contains(new) then
-        reportComplete = false
-        exitRequested = false
+
+    -- If we're not entering or leaving Sortie we want to establish our baseline zone basically this should always be kamihr.
+    if new == baselineZone and not validZones:contains(old) then
         packets.inject(packets.new('outgoing', 0x115))
+        reportComplete = false
+        baselineRequested = true
+        exitRequested = false
     end
 
     -- Entering Sortie from a non-Sortie zone starts a new run.
-    if validZones:contains(new) and baselineZone == old then
-        print('entered sortie')
+    if validZones:contains(new) and old ~= oldZone then
+        reportComplete = false
+        exitRequested = false
+        packets.inject(packets.new('outgoing', 0x115))
     end
 
     -- This is used to determine when we leave Sortie.
@@ -66,17 +70,14 @@ windower.register_event('incoming chunk', function(id, data)
 
     if id == 0x118 then
         local p = packets.parse('incoming', data)
-
         -- Baseline is captured after a baseline request.
         if baselineRequested and not exitRequested then
             initialGalli = p['Gallimaufry']
             log('%d':format(p['Gallimaufry']))
-            baselineRequested = false
         -- We're checking if the current zone isn't a Sortie zone, if we leave Sortie by some means timeout, warp or whatever.
-        elseif exitRequested and not validZones:contains(newZone) then
+        elseif exitRequested then
             totalGalli = p['Gallimaufry']
             log('You have gained %d Muffins!':format(totalGalli - initialGalli))
-            exitRequested = false
 
             local export = io.open(file_path .. 'Totals' .. '.log', 'a')
             if export then
@@ -86,6 +87,11 @@ windower.register_event('incoming chunk', function(id, data)
                 log('Failed to open data/Totals.log for writing')
             end
             log('Wrote Muffins total to data/Totals.log')
+            -- We only reset the baselineRequested to false after we write the log.
+            -- Reset exitRequested to false after written to log.
+            exitRequested       = false
+            baselineRequested   = false
+            reportComplete      = true
         end
     end
 end)
