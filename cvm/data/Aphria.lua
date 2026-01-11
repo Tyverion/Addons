7b3a90a9465ad021f62274f5a27bb51aad85f794
Ender <60693808+Tyverion@users.noreply.github.com>
2026-01-11 02:27:29 -0700

    Add files via upload

---
+++ cvm/data/Aphria.lua
@@ -0,0 +1,10 @@
+return {
+    wanted = {
+        -- Medicine (opt=1)
+        ['Panacea']        = { qty = 12,  slot = 11, opt = 1 },
+        ['Echo Drops']     = { qty = 12,  slot = 13, opt = 1 },
+        ['Remedy']         = { qty = 12,  slot = 16, opt = 1 },
+        -- Foodstuffs
+        ['Grape Daifuku']  = { qty = 12, slot = 67, opt = 4 },
+    }
+}
---
+++ cvm/data/Tyverion.lua
@@ -0,0 +1,19 @@
+return {
+    wanted = {
+        -- Medicine (opt=1)
+        ['Panacea']        = { qty = 24,  slot = 11, opt = 1 },
+        ['Echo Drops']     = { qty = 12,  slot = 13, opt = 1 },
+        ['Antacid']        = { qty = 12,  slot = 14, opt = 1 },
+        ['Holy Water']     = { qty = 12,  slot = 15, opt = 1 },
+        ['Remedy']         = { qty = 12,  slot = 16, opt = 1 },
+        ['Prism Powder']   = { qty = 12,  slot = 18, opt = 1 },
+        ['Silent Oil']     = { qty = 12,  slot = 19, opt = 1 },
+        ['Reraiser']       = { qty = 1,   slot = 21, opt = 1 },
+        ['Hi-Reraiser']    = { qty = 1,   slot = 22, opt = 1 },
+        ['Vile Elixir']    = { qty = 1,   slot = 23, opt = 1 },
+        ['Vile Elixir +1'] = { qty = 1,   slot = 24, opt = 1 },
+
+        -- Foodstuffs
+        ['Grape Daifuku']  = { qty = 12, slot = 67, opt = 4 },
+    }
+}
