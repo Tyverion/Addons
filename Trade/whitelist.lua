321c056093728f824c3d382bd37f0e1fe9b8e2ce
Ender <60693808+Tyverion@users.noreply.github.com>
2026-01-29 00:09:05 -0700

    Update whitelist.lua

--- Trade/whitelist.lua
+++ Trade/whitelist.lua
@@ -1,14 +1,4 @@
 return {
-    ["yolizero"] = { id = 457869, last_seen = 1750581452 },
-    ["aphria"] = { id = 536720, last_seen = 1750581443 },
-    ["kyohyi"] = { id = 174734, last_seen = 1766435650 },
-    ["reizero"] = { id = 421499, last_seen = 1750581444 },
-    ["yuzzie"] = { id = 459739, last_seen = 1750581445 },
-    ["lunarian"] = { id = 154260, last_seen = 1763371406 },
-    ["vanillapod"] = { id = 33798, last_seen = 1767252424 },
-    ["muffuletta"] = { id = 677859, last_seen = 1767169986 },
-    ["johnconnor"] = { id = 596746, last_seen = 1763872253 },
-    ["tyverion"] = { id = 382951, last_seen = 1750581440 },
-    ["trippy"] = { id = 550760, last_seen = 1766054020 },
-    ["melindaconor"] = { id = 563219, last_seen = 1766060217 },
+
 }
+
